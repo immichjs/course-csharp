e13198f04c1fe103b8654a87ca7008f5e7ad89c9
Michel França <fr.mich17@gmail.com>
2021-04-23 12:52:51 -0300

    Aprendendo C#.

---
+++ LearningCSharp/firstProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace firstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro programa em C#");
+
+            Console.WriteLine("Execução do programa terminou. Tecle Enter para finalizar");
+            Console.ReadLine();
+        }
+
+    }
+}
